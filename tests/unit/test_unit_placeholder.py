5d6a14f6f8c5d0e4df37ccdfb91f6d32441929ba
Andrey Reyes <andrey8src@gmail.com>
2026-03-23 01:15:19 -0400

    test(repo): remove placeholder scaffold tests

--- tests/unit/test_unit_placeholder.py
+++
@@ -1,6 +0,0 @@
-"""Placeholder unit tests for the model_foundry package."""
-
-
-def test_placeholder_unit() -> None:
-    """Sanity check that unit tests are being discovered and executed."""
-    assert True
